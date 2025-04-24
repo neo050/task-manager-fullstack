733135fd542c0d9fca7851785c2d8838a392e4b4
neo050 <neoray.asking666@gmail.com>
2025-04-24 20:21:01 +0300

    feat(client): add react router with Home and Login routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
